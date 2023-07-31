5fa751b733d2ed39acb711080265bbcd2d326d57
旭陌小生 <1726010671@qq.com>
2023-07-31 09:42:09 +0800

    项目初始化

---
+++ admin/src/components/layout/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="user-box">layout</div>
+</template>
+
+<script>
+export default {
+    props: {},
+    components: {},
+    computed: {},
+    created() {
+    },
+    data() {
+        return {}
+    },
+    methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
